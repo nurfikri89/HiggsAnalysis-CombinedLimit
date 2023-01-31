b749d2dd7c1f89e927caf20c28540b36560e4b86
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2023-01-31 07:01:29 +0100

    Make sure to setup PATH, LD_LIBRARY_PATH and PYTHONPATH  once in a shell session

--- env_lcg.sh
+++ env_lcg.sh
@@ -1,4 +1,16 @@
 . /cvmfs/sft.cern.ch/lcg/views/LCG_102a/x86_64-centos7-gcc11-opt/setup.sh
-export PATH=${PATH}:${PWD}/build/bin
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/build/lib
-export PYTHONPATH=${PYTHONPATH}:${PWD}/build/lib/python
+
+if [[ !  ${PATH} =~ ${PWD}/build/bin ]]; then 
+  echo "Appending ${PWD}/build/bin to PATH"
+  export PATH=${PATH}:${PWD}/build/bin
+fi
+
+if [[ !  ${LD_LIBRARY_PATH} =~ ${PWD}/build/lib ]]; then 
+  echo "Appending ${PWD}/build/lib to LD_LIBRARY_PATH"
+  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/build/lib
+fi
+
+if [[ !  ${PYTHONPATH} =~ ${PWD}/build/lib/python ]]; then 
+  echo "Appending ${PWD}/build/lib/python to PYTHONPATH"
+  export PYTHONPATH=${PYTHONPATH}:${PWD}/build/lib/python
+fi
